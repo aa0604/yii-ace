8bc9a24e4ba950600f8463643725cb2a9b3a9bcd
aa0604 <aa0604@sina.cn>
2019-07-23 13:14:57 +0800

    初始化

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace xing\ace;
+
+/**
+ * ace module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'xing\ace\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
